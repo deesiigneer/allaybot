08d26ff8af82124fd883e6ec5d697e730ff56081
deesiigneer <goldenrump@gmail.com>
2024-01-26 19:54:12 +0500

    length fix

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ class Bot(commands.Bot):
     async def on_guild_join(self, guild: nextcord.Guild):
         log_guild = self.get_guild(850091193190973472)
         log_channel = log_guild.get_channel(1103024684003508274)
-        content = f'Добавлен на сервер `{guild.name}` ({guild.id}) \n||{[invite for invite in await guild.invites()]}||'
+        content = f'Добавлен на сервер `{guild.name}` ({guild.id}) | {guild.member_count} \n||{[invite.url for invite in await guild.invites()]}||'
         content = f'{content[:1997]}...' if len(content) > 2000 else content
         print(f'content len = {len(content)}')
         await log_channel.send(content)
